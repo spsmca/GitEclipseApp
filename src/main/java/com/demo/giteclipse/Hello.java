eca4b2b58f2ccfd9dd0250e575747596cb6b4623
spsmca <sps_mca@rediffmail.com>
2020-09-27 17:20:38 +0530

    new

---
+++ src/main/java/com/demo/giteclipse/Hello.java
@@ -0,0 +1,11 @@
+package com.demo.giteclipse;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+        System.out.println("Philip Philip:Branch1....");
+
+	}
+
+}
--- src/main/java/com/demo/giteclipse/Welcome.java
+++ src/main/java/com/demo/giteclipse/Welcome.java
@@ -5,7 +5,10 @@ public class Welcome {
 	public static void main(String[] args) {
 
         System.out.println("Welcome Philip:Branch1....");
+        
+        System.out.println("Welcome Philip:Branch1....Modified in Local...");
 
 	}
 
 }
+
