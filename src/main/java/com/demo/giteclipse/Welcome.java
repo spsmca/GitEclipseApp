1aab11c2aec90c5bd8d7c55b21ef690e47197b61
spsmca <71463984+spsmca@users.noreply.github.com>
2020-09-27 19:28:18 +0530

    Update Welcome.java

--- src/main/java/com/demo/giteclipse/Welcome.java
+++ src/main/java/com/demo/giteclipse/Welcome.java
@@ -8,7 +8,7 @@ public class Welcome {
         
         System.out.println("Welcome Philip:Branch1....Modified in Local..);
 
-        System.out.println("Welcome Philip:Branch1....Modified in Remote...);
+        System.out.println("Welcome Philip:Branch1....Modified in Remote...BB);
 
 	}
 
