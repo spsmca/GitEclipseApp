c96d6598e26e04b7bae09430a536b6ef2a0e1afe
spsmca <sps_mca@rediffmail.com>
2020-09-27 19:10:25 +0530

    Commiting Conflict

---
+++ src/main/java/com/demo/giteclipse/Hello.java
@@ -0,0 +1,14 @@
+package com.demo.giteclipse;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+        System.out.println("Hello Philip:Branch1....");
+        
+        System.out.println("Hello Philip:Branch1....Modified in Local...);
+
+	}
+
+}
+
