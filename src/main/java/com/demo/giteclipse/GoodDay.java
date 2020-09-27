1d74148ca690f6437b0e35bb331ad9eb24d4f42a
spsmca <71463984+spsmca@users.noreply.github.com>
2020-09-27 18:55:18 +0530

    Create GoodDay.java

---
+++ src/main/java/com/demo/giteclipse/GoodDay.java
@@ -0,0 +1,13 @@
+package com.demo.giteclipse;
+
+public class GoodDay{
+
+	public static void main(String[] args) {
+
+        System.out.println("GoodDay Philip:Branch1....");
+        
+        System.out.println("GoodDay Philip:Branch1....Modified in Local...);
+
+	}
+
+}
